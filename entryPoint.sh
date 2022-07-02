7a3b6bc09d5537dfd937e7ba54c645b77c44ac99
furkan-guvenc <furkan.gvenc@gmail.com>
2022-07-02 12:07:05 +0100

    chore: change plane to copter

--- entryPoint.sh
+++ entryPoint.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-echo SYSID_THISMAV=$1 | tee -a /ardupilot/Tools/autotest/default_params/plane.parm
+echo SYSID_THISMAV=$1 | tee -a /ardupilot/Tools/autotest/default_params/copter.parm
 cat /external/extra-locations.txt >> /ardupilot/Tools/autotest/locations.txt
 START_LOCATION=$(< /external/start-location.conf)
-sim_vehicle.py -N -v ArduPlane -L $START_LOCATION $SIM_OPTIONS 
+sim_vehicle.py -N -v ArduCopter -L $START_LOCATION $SIM_OPTIONS
