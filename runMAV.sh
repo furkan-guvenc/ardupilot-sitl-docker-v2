e408a372a36b962732689f1e1e45fe5a97e86cb8
furkan-guvenc <furkan.gvenc@gmail.com>
2022-07-03 19:30:32 +0100

    add unlimited destination to mavproxy

--- runMAV.sh
+++ runMAV.sh
@@ -1,22 +1,29 @@
 #! /bin/bash
 
-if [ $# -ne 3 ] 
+if [ $# -ne 1 ]
 then
-  echo "Usage: runMAV.sh <system_id> <gcs_host> <gcs_port>"
+  echo "Usage: runMAV.sh <system_id> <gcs_host1>:<gcs_port1> <gcs_host2>:<gcs_port2> ..."
   exit 1
 fi
 
 SYS_ID=$1
-GCS_HOST=$2
-GCS_PORT=$3
 SCRIPTS_DIR=$(cd $(dirname $(which $0)); pwd)
+
+DESTINATIONS=""
+
+echo "Number of destinations:" $(( $# - 1 ))
+for src in ${@:2}
+do
+    DESTINATIONS+="--out=udpout:${src} "
+done
+echo $DESTINATIONS
 
 echo "Running SITL simulation ..."
 echo SYS_ID=$SYS_ID GCS_HOST=$2 GCS_PORT=$3
 echo SIM_OPTIONS=$SIM_OPTIONS
 docker run --rm -it \
   -v $SCRIPTS_DIR:/external \
-  -e "SIM_OPTIONS=--out=udpout:$GCS_HOST:$GCS_PORT --out=udpout:$GCS_HOST:14550 -m --target-system=$SYS_ID $SIM_OPTIONS" \
+  -e "SIM_OPTIONS=$DESTINATIONS -m --target-system=$SYS_ID $SIM_OPTIONS" \
   --entrypoint "/external/entryPoint.sh" \
   furkanguvenc/ardupilot-sitl:latest $SYS_ID
 exit $?
